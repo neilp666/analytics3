f4808dca9f7c8846aec98cbfef9525141ffb6940
neil patel <neil@myindianweddingapp.com>
2014-07-28 21:15:59 +0100

    Updated Show File

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,11 +1,11 @@
 class Listing < ActiveRecord::Base
 
 	if Rails.env.development?
-		has_attached_file :image, :styles => { :thumb => "190x190>" }, :default_url => "missing.png"
+		has_attached_file :image, :styles => { :thumb => "150x150>" }, :default_url => "missing.png"
 	    validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
 
 	else
-		has_attached_file :image, :styles => { :thumb => "190x190"},
+		has_attached_file :image, :styles => { :thumb => "150x150"},
     	:storage => :dropbox,
    	    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
    	    :path => ":style/id_:filename"
